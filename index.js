b4af3422e43362e107567321a9a363699600b25f
Tony Guzman <tlguzmandev@gmail.com>
2024-01-16 13:16:42 -0500

    eslint

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-console */
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
@@ -9,84 +11,87 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-morgan.token('body', req => {
-    return JSON.stringify(req.body)
-})
+morgan.token('body', (req) => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/info', (req, res) => {
-    res.send(`<h1>Phonebook has info for ${data.length} people</h1><p>${new Date().toUTCString()}</p>`)
+app.get('/info', (req, res, next) => {
+  Person.countDocuments({}).count()
+    .then((result) => {
+      res.send(`<h1>Phonebook has info for ${result} people</h1><p>${new Date().toUTCString()}</p>`)
+    })
+    .catch((err) => next(err))
 })
 
 app.get('/api/persons', (req, res, next) => {
-    Person.find({})
-        .then(persons => {
-            res.json(persons)
-        })
-        .catch(err => next(err))
+  Person.find({})
+    .then((persons) => {
+      res.json(persons)
+    })
+    .catch((err) => next(err))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
-        .then(person => {
-            if (person) {
-                res.json(person)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(err => next(err))
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch((err) => next(err))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndDelete(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(err => next(err))
+  Person.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch((err) => next(err))
 })
 
 app.post('/api/persons', (req, res) => {
-    const body = req.body
-    
-    if(!body.name || !body.number) {
-        return res.status(400).json({
-            error: 'content missing include a name and number'
-        })
-    }
+  const { body } = req
 
-    const person = new Person({
-        name: body.name,
-        number: body.number
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'content missing include a name and number',
     })
+  }
 
-    person.save().then(savedPerson => {
-        res.json(savedPerson)
-    })
-    
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then((savedPerson) => {
+    res.json(savedPerson)
+  })
 })
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({error: 'unknown endpoint'})
+  res.status(404).send({ error: 'unknown endpoint' })
 }
 
 const errorHandler = (err, req, res, next) => {
-    console.error(err.message)
+  console.error(err.message)
 
-    if( err.name === 'CastError') {
-        return res.status(400).send({error: 'malformatted id'})
-    } else if (err.name === 'ValidationError') {
-        return res.status(400).json({error: err.message})
-    }
+  if (err.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
 
-    next(err)
+  if (err.name === 'ValidationError') {
+    return res.status(400).json({ error: err.message })
+  }
+
+  next(err)
 }
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const { PORT } = process.env
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
